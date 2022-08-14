746ec0ae9cff95de7cd9ee7dedfc17cc414235e3
Joshua Schmitt <jqshuv@gmail.com>
2022-08-14 16:44:43 +0200

    Update main.go

--- src/main.go
+++ src/main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/tawesoft/golib/v2/dialog"
 
 func main() {
-	err := dialog.Info("Hello World")
+	err := dialog.Info("This is an example Programm.")
 	if err != nil {
 		return
 	}
